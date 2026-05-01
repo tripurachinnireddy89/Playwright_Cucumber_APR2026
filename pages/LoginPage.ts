7b6e60906cfccc066ac3d325795703fedb7ba770
tripurachinnireddy89 <tripurachinnireddy@gmail.com>
2026-05-01 17:57:36 +0530

    add the more wait

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -16,6 +16,7 @@ export default class LoginPage extends BasePage {
   }
 
   async isDashboardVisible() {
-   await this.highlight(selectors.dashboard);
+    await this.page.waitForSelector(selectors.dashboard, { timeout: 15000 });
+    await this.highlight(selectors.dashboard);
   }
 }
